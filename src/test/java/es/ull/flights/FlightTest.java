7fc73301fae9d11c7b41e4620cb3bf1fcbe62e0a
David Hernández Lozano <72358101+alu0101361093@users.noreply.github.com>
2022-12-12 09:50:00 +0000

    second commit to launch sonarcloud analysis

--- src/test/java/es/ull/flights/FlightTest.java
+++ src/test/java/es/ull/flights/FlightTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("Given there is a flight")
+@DisplayName("Given there are flights")
 public class FlightTest {
 
     private Flight flight1;
